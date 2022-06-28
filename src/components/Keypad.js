c7de48da3c31e60bcd1ea4149de721c4bb15ad84
Loyda <palacioloydita@gmail.com>
2022-06-27 20:21:33 -0700

    pushing code

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+
+import React from 'react';
+
+function Keypad () {
+    function handleClick(event) {
+        console.log("Entering password...")
+
+    }
+
+    return(
+        <div>
+            <input 
+                type="password"
+                name="password"
+                onChange={handleClick}
+                placeholder="Enter password..."
+            />
+        </div>
+    )
+}
+export default Keypad;
